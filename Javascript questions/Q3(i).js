7c9784aad37e89800c0a2019d1f4723b414079ca
rohan <rohankenjale50@gmail.com>
2023-05-12 22:30:50 +0530

    appli

---
+++ Javascript questions/Q3(i).js
@@ -0,0 +1,45 @@
+function fetchStudents() {
+    fetch('https://1qy9d2uuyg.execute-api.ap-northeast-1.amazonaws.com/default/Students-API')
+      .then(response => response.json())
+      .then(students => {
+        students.sort((a, b) => a.name.localeCompare(b.name));
+        for (const student of students) {
+            console.log(student.name);
+          }
+      })
+      .catch(error => console.error(error));
+  }
+// fetchStudents()
+function fetchStudentsByCollege(college) {
+    fetch('https://1qy9d2uuyg.execute-api.ap-northeast-1.amazonaws.com/default/Students-API')
+      .then(response => response.json())
+      .then(students => {
+        const filteredStudents = students.filter(student => student.college === college);
+        for (const student of filteredStudents) {
+            console.log(student.name);
+          };
+      })
+      .catch(error => console.error(error));
+  }
+// fetchStudentsByCollege('Brigham Young University')
+const express = require('express');
+const app = express();
+const PORT = 3001; // Replace with your desired port number
+
+// Endpoint for sorting the list of students
+app.get('/students/sort', async (req, res) => {
+  const students = await fetchStudents();
+  res.json(students);
+});
+
+// Endpoint for filtering the list of students by college
+app.get('/students/filter/:college', async (req, res) => {
+  const college = req.params.college;
+  const filteredStudents = fetchStudentsByCollege(college);
+  res.json(filteredStudents);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ flutter app questions/android/app/src/main/kotlin/com/example/cfiwebops/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cfiwebops
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
